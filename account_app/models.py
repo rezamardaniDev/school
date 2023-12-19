6c89d691ad927510b170774a85fbbd9d548289c0
reza <mardanireza58@gmail.com>
2023-12-19 14:08:14 +0330

    write model for user

--- account_app/models.py
+++ account_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255, verbose_name="نام")
+    last_name = models.CharField(max_length=255, verbose_name="نام خانوادگی")
+    age = models.CharField(max_length=2, verbose_name="سن")
+    username = models.CharField(max_length=255, unique=True, verbose_name="نام کاربری")
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کابران"
