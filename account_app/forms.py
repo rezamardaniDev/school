ae7841148df2d170f22d3e04776a66f60a8550c5
reza <mardanireza58@gmail.com>
2023-12-19 14:10:41 +0330

    write form for login and signup

---
+++ account_app/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(
+        label='نام',
+        widget=forms.TextInput()
+    )
+    last_name = forms.CharField(
+        label='نام خانوادگی',
+        widget=forms.TextInput()
+    )
+    age = forms.CharField(
+        label='سن',
+        widget=forms.TextInput()
+    )
+    username = forms.CharField(
+        label='نام کاربری',
+        widget=forms.TextInput()
+    )
+    password = forms.CharField(
+        label="پسورد",
+        widget=forms.TextInput()
+    )
+    confirm_password = forms.CharField(
+        label="تکرار پسورد",
+        widget=forms.TextInput()
+    )
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+
+        if password == confirm_password:
+            return confirm_password
+        else:
+            raise forms.ValidationError("رمز عبور های وارد شده یکسان نیستند")
+
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        label='نام کاربری',
+        widget=forms.TextInput(),
+        error_messages={
+            'required': 'نام کاربری خود را وارد کنید'
+        }
+    )
+    password = forms.CharField(
+        label='پسورد',
+        widget=forms.TextInput(),
+        error_messages={
+            'required': 'پسورد خود را وارد کنید'
+        }
+    )
